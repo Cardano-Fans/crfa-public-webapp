880e3c2ea005f7b8ce1a165437919907327afac8
Mateusz Czeladka <mateusz.szczap@gmail.com>
2022-01-29 14:00:14 +0100

    poolId change

--- components/home/about-pool/Delegation.tsx
+++ components/home/about-pool/Delegation.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { Slide } from './Slide'
 import { copyToClipboard } from '../../../utils/copyToClipboard'
 
-const POOL_ID = '6C518B4861BB88B1395CEB116342CECBCFB8736282655F9A61C4C368'
+const POOL_ID = '6c518b4861bb88b1395ceb116342cecbcfb8736282655f9a61c4c368'
 
 export const Delegation: React.FC = () => {
   const [isCopied, setIsCopied] = useState(false)
